2846efe90526b364d88722c6e1262011427b6c50
Jimmy Hong <jimmyhong.rocks@gmail.com>
2018-04-07 16:17:57 -0500

    Fix gitignore to include dist files

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'FSG' });
 });
 
 /* GET test page. */
-router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Test Page' });
-});
+// router.get('/test', function(req, res, next) {
+//   res.render('test', { title: 'Test Page' });
+// });
 
 module.exports = router;
